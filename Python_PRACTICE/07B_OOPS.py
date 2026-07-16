dcd90de7e07bfaf10a241052631d13c1fe9832bc
opbihari <opbihari@users.noreply.github.com>
2026-07-16 14:57:27 +0530

    main

--- Python_PRACTICE/07B_OOPS.py
+++ Python_PRACTICE/07B_OOPS.py
@@ -2,22 +2,7 @@ import csv
 
 loan_data = "loan_data.csv"
 
-old_user = False
-
-        
-while True:
-    try:
-        phone_number = int(input("Enter the phone number  "))
-        if len(phone_number) == 10:
-            break
-        else:
-            print("Phone number should be 10 digits")
-    except:
-        print("Invalid phone number ")
-
-
-
-    
+old_user = False   
 
 class loan:
     def __init__(self,name,salary,loan_amount,phone_number,credit_score):
@@ -26,21 +11,18 @@ class loan:
         self.loan_amount = loan_amount
         self.credit_score = credit_score
         self.phone_number = phone_number
-    def load_data(self, loan_data):
+    def search_phone(self, phone_number):
         with open(loan_data, mode="r", encoding="utf-8", newline="") as loan_data_file:
             data_reader = csv.reader(loan_data_file)
-            return data_reader
-
-    def search_phone(self, phone_number):
-        data_reader = self.loan_data(loan_data)
-        for row in data_reader:
-            if row[3] == phone_number:
-                print("Phone Number Found")
-                old_user = True
-                info = row[0],row[1],row[2],row[3],row[4]
-            print(f"name {info[0]}\nsalary {info[1]}\nLoan Amount {info[2]}\nphone number {info[3]}\ncredit score {info[4]}")
-       
-    
+            for row in data_reader:
+                if row[3] == phone_number:
+                    print("Phone Number Already Exists")
+                    old_user = True
+                    info = row[0],row[1],row[2],row[3],row[4]
+                    print(f"name {info[0]}\nsalary {info[1]}\nLoan Amount {info[2]}\nphone number {info[3]}\ncredit score {info[4]}")
+                    break
+                else:
+                    print("enter details")
     def requirements(self):
         if self.salary >= 20000:
             print(f"Salary  {self.salary} ✅ ")
@@ -67,7 +49,42 @@ class loan:
            print(f"Name : {self.name}")
            self.requirements()
            print("Loan Rejected")
-        
+    def new_user(self):
+        name = input("Enter the name ")
+        salary = int(input("Enter the salary "))
+        loan_amount = int(input("Enter the loan amount "))
+        credit_score = int(input("Enter the credit score "))
+        loan1 = loan(name,salary,loan_amount,phone_number,credit_score)
+        loan1.loan_evalute()
+        with open(loan_data,"a",encoding="utf-8",newline="") as loan_data_file:
+            data_writer = csv.writer(loan_data_file)
+            data_writer.writerow([name,salary,loan_amount,phone_number,credit_score])
 
-loan1 = loan("Shiva-kumar",1000,10000,676464564,720)
-loan1.loan_evalute()
+if __name__ == "__main__":
+    while True:
+        try:
+            phone_number = (input("Enter the phone number  ")).strip()
+            if len(phone_number) == 10 and phone_number.isdigit():
+                break
+            else:
+                print("Phone number should be 10 digits")
+        except:
+            print("Invalid phone number ")
+    loan.search_phone(phone_number)
+    if old_user == False:
+        loan.new_user(phone_number)
+    elif old_user == True:
+        while True:
+            try:
+                choise_1 = int(input("press enter to evalute or 1 to update detail"))
+                if choise_1 == 1:
+                    break
+                elif not choise_1:
+                    pass
+                    
+
+        
+        
+            
+            
+        
